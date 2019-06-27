db03f9c7b6c199fbeaff69dd941fa121f7908e19
RangerMauve <ranger@mauve.moe>
2019-06-27 18:15:45 -0400

    Initial draft of what unit tests should exist

---
+++ test.js
@@ -0,0 +1,27 @@
+const test = require('tape')
+
+module.exports = ({
+  // Initialize whatever is needed
+  init,
+  createPeer,
+  createFakeID,
+  lookup,
+  connect,
+  getGraph,
+  destroy
+}) => {
+  test('happy path')
+
+  test('fewer peers than sample size')
+
+  test('more peers than sample size')
+
+  test('unable to connect to peer')
+
+  test('able to connect to second peer')
+
+
+  test('large sample size')
+
+  test('small sample size')
+}
